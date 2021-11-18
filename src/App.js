1ce92304fe0c0e54b9a49cc6b5117aae3fd59976
Jaspreet Singh Kohli <70095662+Jaspreetkohli72@users.noreply.github.com>
2021-11-18 14:17:29 +0530

    updated repo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Header } from "./MyComponents/Header";
+import { Todos } from "./MyComponents/Todos";
+import { Footer } from "./MyComponents/Footer";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	let todos = [
+		{
+			sno: 1,
+			title: "Go to the market",
+			desc: "You need to go to the market to get this job done",
+		},
+		{
+			sno: 3,
+			title: "Go to the mall",
+			desc: "You need to go to the market to get this job done",
+		},
+		{
+			sno: 3,
+			title: "Go to the ghat",
+			desc: "You need to go to the market to get this job done",
+		},
+	];
+	return (
+		<>
+			<Header title="My Todos List" searchBar={false} />
+			<Todos todos={todos} />
+			<Footer />
+		</>
+	);
 }
 
 export default App;
---
+++ src/MyComponents/Todos.js
@@ -0,0 +1,10 @@
+import React from "react";
+import TodoItem from "./TodoItem";
+
+export const Todos = (props) => {
+	return (
+	<div className="container">
+		<h3>Todos List</h3>
+		<TodoItem todo={props.todo[0]} />
+	</div>);
+};
